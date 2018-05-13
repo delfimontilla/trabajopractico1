a72052591abe1349c07db3d2d1ce8b47e21000d9
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-13 20:12:14 -0300

    Correcciones
    correcion de *contador_programa++; por lo comentado por pato

--- operacion/djnz.c
+++ operacion/djnz.c
@@ -1,7 +1,9 @@
-status_t djnz (int acumulador, int * contador_programa,  int operando)/*decrementa en 1 el acumulador y salta a la poscion deseada menos 1 en el caso que el acumulador sea distinto de 0*/{
+status_t djnz (int acumulador, int * contador_programa,  int operando)/*decrementa en 1 el acumulador y salta a la poscion deseada en el caso que el acumulador sea distinto de 0*/{
 	acumulador--;
 	if (acumulador!=0){
-		* contador_programa = operando-1;/*le resto uno para que cuando salgo del switch y le sume 1 quede donde quiero*/
+		* contador_programa = operando;
 	}
+	else
+		*contador_programa++;
 	return ST_OK;
 }
