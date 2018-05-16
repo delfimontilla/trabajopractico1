b022d80efc2fb0ba397efbbabee7b59b11786068
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:10:35 -0300

    parametros_t->

--- operacion/dividir.c
+++ operacion/dividir.c
@@ -1,4 +1,4 @@
-status_t dividir (int acumulador, int  palabras[cant_palabras] , int operando)/*divide al acumulador lo guardado en la posicion de memoria indcada*/{
+status_t dividir (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*divide al acumulador lo guardado en la posicion de memoria indcada*/{
 	acumulador /= palabras[operando];
 	return ST_OK;
 }
