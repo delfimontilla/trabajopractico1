7028952474f7890146ec5fcff0105ec7b82b168a
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:23:31 -0300

    params

--- operacion/leer.c
+++ operacion/leer.c
@@ -1,4 +1,4 @@
-status_t leer (int operando, int palabras[parametros_t->cant_palabras] )/*Lee una palabra por stdin a una posicion de memoria*/{
+status_t leer (int operando,parametros_t *params, int palabras[params->cant_palabras] )/*Lee una palabra por stdin a una posicion de memoria*/{
 	long * AUX;
 	fprintf(stdout, "%s\n", MSJ_INGRESO_PALABRA);
 	if (fgets(AUX,MAX_LARGO_PALABRA,stdin)==NULL)
