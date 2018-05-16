fdbac2b51ba32fadd8fbd0e255f0df07905ba20f
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:11:14 -0300

    parametros_t->

--- operacion/guardar.c
+++ operacion/guardar.c
@@ -1,4 +1,4 @@
-status_t guardar (int acumulador, int  palabras[cant_palabras] , int operando)/*guarda en la posisicon de memoria indicada lo que esta en el acumulador*/{
+status_t guardar (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*guarda en la posisicon de memoria indicada lo que esta en el acumulador*/{
 	palabras[operando] = acumulador ;
 	return ST_OK;
 }
