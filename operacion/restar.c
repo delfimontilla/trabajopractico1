638ddb15e2bcbb4b80f57d6828ef75ce0c43d741
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:13:40 -0300

    parametros_t->

--- operacion/restar.c
+++ operacion/restar.c
@@ -1,4 +1,4 @@
-status_t restar (int acumulador, int  palabras[cant_palabras] , int operando)/*resta al acumulador lo guardado en la posicion de memoria indcada*/{
+status_t restar (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*resta al acumulador lo guardado en la posicion de memoria indcada*/{
 	acumulador -= palabras[operando];
 	return ST_OK;
 }
