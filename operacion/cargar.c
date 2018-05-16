1935c708ef561f95b9b657051321eabf2162c343
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:21:55 -0300

    params

--- operacion/cargar.c
+++ operacion/cargar.c
@@ -1,4 +1,4 @@
-status_t cargar (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*Carga en el acumulador la posicion de memoria indicada*/{
+status_t cargar (int acumulador, parametros_t *params, int palabras[params->cant_palabras], int operando)/*Carga en el acumulador la posicion de memoria indicada*/{
 	acumulador = palabras[operando];
 	return ST_OK;
 }
