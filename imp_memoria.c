23342c726155407c1ce6a2298f8e7aaec2950595
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-15 21:17:03 -0300

    Registro e imprimir memoria

---
+++ imp_memoria.c
@@ -0,0 +1,20 @@
+
+status_t imp_memoria (int  palabras[cant_palabras] /*vector donde estan guardadas las palabras*/){
+	int i,j,k,l;
+	k=0;
+	for (int l = 0; l < 10; l++){
+		printf(" %i ",l );
+	}
+	printf("\n");
+	for ( i = 0; i < cant_palabras; i++){
+		for ( j = 0; j < 10; j++){
+			if(palabras[i]<0)
+				printf("-%4i ",palabras[i] );
+			else 
+				printf("+%4i ",palabras[i] );
+		}
+	printf("\n");
+	printf("%i\n",0 );
+	k+=10;
+	}
+}
