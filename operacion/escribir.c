46c5bdd079079d3b7f5be76ff1abb154341f64ab
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:15:48 -0300

    parametros_t_->

--- operacion/escribir.c
+++ operacion/escribir.c
@@ -1,5 +1,3 @@
-#define MSJ_IMPRIMIR_PALABRA "Contenido de la posicion"
-
 status_t escribir(int operando, int  palabras[parametros_t->cant_palabras] )/*imprime por stdout el contenido de la posicion solicitada*/{
 	fprintf(stdout, "%s %i : %i\n", MSJ_IMPRIMIR_PALABRA,operando, palabras[operando]);
 	return ST_OK;
