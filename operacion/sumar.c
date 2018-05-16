61bddb26f9a193a2ef81b02a33d915d4b68ddd5d
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:24:48 -0300

    params

--- operacion/sumar.c
+++ operacion/sumar.c
@@ -1,4 +1,4 @@
-status_t sumar (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*suma al acumulador lo guardado en la posicion de memoria indcada*/{
+status_t sumar (int acumulador, parametros_t *params, int palabras[params->cant_palabras] , int operando)/*suma al acumulador lo guardado en la posicion de memoria indcada*/{
 	acumulador += palabras[operando];
 	return ST_OK;
 }
