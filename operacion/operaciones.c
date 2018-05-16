8906d60966e7774ab886e8b68eafc4c5d1d15755
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:13:21 -0300

    parametros_t->

--- operacion/operaciones.c
+++ operacion/operaciones.c
@@ -9,7 +9,7 @@ int main (lo que sea){
 */
 status_t operaciones ( 
 int * acumulador /*posicion de memoria del acumulador*/,
-int  palabras[cant_palabras] /*vector donde estan guardadas las palabras*/
+int  palabras[parametros_t->cant_palabras] /*vector donde estan guardadas las palabras*/
 int * contador_programa /*cuenta el numero de paso y de memoria que se encuentra el programa*/)
 /*Funcion que recibe las dos primeras posiciones de la posicion y decide que operacion correr*/
 {
@@ -25,19 +25,19 @@ int * contador_programa /*cuenta el numero de paso y de memoria que se encuentra
 		operando = palabras[contador_programa] - (opcode*100);/*necesito los ultimos dos entonces al multiplicar opcode por 100 tengo 2500 del ejemplo entonces 2598-2500 da 98 que son los ultimos dos digitos que necesito*/
 		switch (opcode){
 			case (LEER):
-				leer(operando, palabras[cant_palabras] );
+				leer(operando, palabras[parametros_t->cant_palabras] );
 				*contador_programa++;
 				break;
 			case (ESCRIBIR):
-				escribir(operando, palabras[cant_palabras] );
+				escribir(operando, palabras[parametros_t->cant_palabras] );
 				*contador_programa++;
 				break;
 			case (CARGAR):
-				cargar(&acumulador, palabras[cant_palabras], operando );
+				cargar(&acumulador, palabras[parametros_t->cant_palabras], operando );
 				*contador_programa++;
 				break;
 			case (GUARDAR):
-				guardar(&acumulador, palabras[cant_palabras],operando );
+				guardar(&acumulador, palabras[parametros_t->cant_palabras],operando );
 				*contador_programa++;
 				break;
 			case (PCARGAR):
@@ -49,19 +49,19 @@ int * contador_programa /*cuenta el numero de paso y de memoria que se encuentra
 				*contador_programa++;
 				break;
 			case(SUMAR):
-				sumar(&acumulador, palabras[cant_palabras],operando );
+				sumar(&acumulador, palabras[parametros_t->cant_palabras],operando );
 				*contador_programa++;
 				break;
 			case(RESTAR):
-				restar(&acumulador, palabras[cant_palabras],operando );
+				restar(&acumulador, palabras[parametros_t->cant_palabras],operando );
 				*contador_programa++;
 				break;
 			case(DIVIDIR):
-				dividir(&acumulador, palabras[cant_palabras],operando );
+				dividir(&acumulador, palabras[parametros_t->cant_palabras],operando );
 				*contador_programa++;
 				break;
 			case(MULTIPLICAR):
-				multiplicar(&acumulador, palabras[cant_palabras],operando );
+				multiplicar(&acumulador, palabras[parametros_t->cant_palabras],operando );
 				*contador_programa++;
 				break;
 			case(JMP):
