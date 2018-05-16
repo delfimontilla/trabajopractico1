f9e8c370cb704e3023e8c9181cef71c949c94640
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:11:49 -0300

    parametros_t->

--- operacion/leer.c
+++ operacion/leer.c
@@ -1,7 +1,4 @@
-#define MAX_LARGO_PALABRA 5 /*maxima cantidad caracteres en una palabra*/
-#define MSJ_INGRESO_PALABRA "Ingrese una palabra:"
-
-status_t leer (int operando, int palabras[cant_palabras] )/*Lee una palabra por stdin a una posicion de memoria*/{
+status_t leer (int operando, int palabras[parametros_t->cant_palabras] )/*Lee una palabra por stdin a una posicion de memoria*/{
 	long * AUX;
 	fprintf(stdout, "%s\n", MSJ_INGRESO_PALABRA);
 	if (fgets(AUX,MAX_LARGO_PALABRA,stdin)==NULL)
