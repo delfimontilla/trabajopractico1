62e16afc4e82dd3716aa31b3a2ff685b3cbc017f
CarlaSobico <37754488+CarlaSobico@users.noreply.github.com>
2018-05-16 16:24:06 -0300

    params

--- operacion/multiplicar.c
+++ operacion/multiplicar.c
@@ -1,4 +1,4 @@
-status_t multiplicar (int acumulador, int  palabras[parametros_t->cant_palabras] , int operando)/*multiplica al acumulador lo guardado en la posicion de memoria indcada*/{
+status_t multiplicar (int acumulador, parametros_t *params, int palabras[params->cant_palabras] , int operando)/*multiplica al acumulador lo guardado en la posicion de memoria indcada*/{
 	acumulador *= palabras[operando];
 	return ST_OK;
 }
