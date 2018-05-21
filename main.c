a2dc9bc0298bc5faed1ea74b386266b7a503ebce
delfimontilla <delfimontilla@users.noreply.github.com>
2018-05-20 21:04:34 -0300

    validar_arg

--- main.c
+++ main.c
@@ -156,13 +156,14 @@ status_t validar_argumentos (int argc , char *argv[], parametros_t *params, esta
 		return ST_ERROR_PTR_NULO;
 	}
 
-	if(argc!=ARGC_MAX){
+	if(argc>ARGC_MAX){
 		fprintf(stderr, "%s: %s\n", MSJ_ERROR, MSJ_ERROR_CANT_ARG );
 		return ST_ERROR_CANT_ARG;
 	}
 
 	if(argv[ARG_POS_CANT_PALABRAS]==NULL){
-		params->cant_palabras=CANT_PALABRAS_DEFAULT;}
+		params->cant_palabras=CANT_PALABRAS_DEFAULT;
+	}
 	else {
 		params->cant_palabras = strtol(argv[ARG_POS_CANT_PALABRAS], &pc, 10);
 		if(params->cant_palabras<0 || (*pc!='\0' && *pc!='\n') || params->cant_palabras>100){
